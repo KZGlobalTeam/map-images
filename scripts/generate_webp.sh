54518f0743400330b65668cf1dcd9c72cfde8c1c
Winston "Ruto <20764435+DevRuto@users.noreply.github.com>
2020-10-09 20:17:56 -0400

    Updated WEBP quality to 90

--- scripts/generate_webp.sh
+++ scripts/generate_webp.sh
@@ -14,8 +14,8 @@ for file in ./images/*.{jpg,jpeg,png,gif}
 do
     name=$(basename -- "$file")
     echo "Generating webp for $name"
-    gm convert -quality 80 "$file" "$OUTPUT/${name%.*}.webp"
-    gm convert -quality 50 -thumbnail 200 "$file" "$OUTPUT/thumb/${name%.*}.webp"
+    gm convert -quality 90 "$file" "$OUTPUT/${name%.*}.webp"
+    gm convert -quality 90 -thumbnail 200 "$file" "$OUTPUT/thumb/${name%.*}.webp"
 done
 
 shopt -u nullglob
