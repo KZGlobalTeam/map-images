4f9fc40f1e990305bcf9e39e076a9d3fbb303ec1
Sikari <OfficialSikari@gmail.com>
2022-01-22 14:42:12 +0200

    Generate "medium sized" webp images

--- scripts/generate_webp.sh
+++ scripts/generate_webp.sh
@@ -5,7 +5,9 @@ OUTPUT="./public/webp"
 if [ -d "$OUTPUT" ]; then
     rm -r "$OUTPUT"
 fi
+
 mkdir -p "$OUTPUT/thumb"
+mkdir -p "$OUTPUT/medium"
 
 shopt -s nullglob
 shopt -s nocaseglob
@@ -16,6 +18,7 @@ do
     echo "Generating webp for $name"
     gm convert -quality 90 "$file" "$OUTPUT/${name%.*}.webp"
     gm convert -quality 90 -thumbnail 200 "$file" "$OUTPUT/thumb/${name%.*}.webp"
+    gm convert -quality 90 -thumbnail 512 "$file" "$OUTPUT/medium/${name%.*}.webp"
 done
 
 shopt -u nullglob
