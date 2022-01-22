a605213cea3b0eaa2aab24178b6f2ea3f674e7dd
Sikari <OfficialSikari@gmail.com>
2022-01-22 15:39:08 +0200

    Shorten width of table to make it nicer on Github

--- scripts/generate_maps_page.sh
+++ scripts/generate_maps_page.sh
@@ -13,8 +13,8 @@ fi
 
 append_md "# MAP IMAGES"
 append_md
-append_md "| Map | Thumbnail | Image Link | Medium Link | Thumbnail Link | WEBP Link | WEBP Medium | WEBP Thumbnail |"
-append_md "|-----|-----------|------------|-------------|----------------|-----------|-------------|----------------|"
+append_md "| Map | Thumbnail | Full | Medium | Thumb | WEBP Full | WEBP Medium | WEBP Thumb |"
+append_md "|-----|-----------|------|--------|-------|-----------|-------------|------------|"
 
 # https://stackoverflow.com/a/41139446
 shopt -s nullglob
@@ -23,7 +23,7 @@ shopt -s nocaseglob
 for file in ./public/thumbnails/*.{jpg,jpeg,png,gif}
 do
     name=$(basename -- "$file")
-    append_md "|${name%.*}|![$name]($PUBLIC_PATH/webp/thumb/${name%.*}.webp)|[image]($PUBLIC_PATH/images/$name)|[medium]($PUBLIC_PATH/mediums/$name)|[thumbnail]($PUBLIC_PATH/thumbnails/$name)|[webp]($PUBLIC_PATH/webp/${name%.*}.webp)|[medium]($PUBLIC_PATH/webp/medium/${name%.*}.webp)|[thumbnail]($PUBLIC_PATH/webp/thumb/${name%.*}.webp)|"
+    append_md "|${name%.*}|![$name]($PUBLIC_PATH/webp/thumb/${name%.*}.webp)|[image]($PUBLIC_PATH/images/$name)|[medium]($PUBLIC_PATH/mediums/$name)|[thumb]($PUBLIC_PATH/thumbnails/$name)|[webp]($PUBLIC_PATH/webp/${name%.*}.webp)|[medium]($PUBLIC_PATH/webp/medium/${name%.*}.webp)|[thumb]($PUBLIC_PATH/webp/thumb/${name%.*}.webp)|"
 done
 
 shopt -u nullglob
